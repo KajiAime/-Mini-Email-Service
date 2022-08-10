8396be652131b6ecc9a25c85843b92f235ec5f1e
Nde-Ebom <ndekaji@gmail.com>
2022-08-10 16:18:50 +0100

    The script now accepts parameters from the page.
    Using the necessary codes, the variables are collected from the page and processed here for operation. Everything is now very dynamic.

--- mails.php
+++ mails.php
@@ -1,16 +1,17 @@
 <?php
    ini_set( 'display_errors', 1 );
    error_reporting( E_ALL );
-   $from = "kajissob@gmail.com";
-   $to = "ndekaji@gmail.com";
-   $subject = "Hello Kaji";
-   $message = "Someone just waved to say hello";
+   $from = $_POST['frome'];
+   $to = $_POST['toe'];
+   $subject = $_POST['subject'];
+   $message = $_POST['messages'];
    $headers = "MIME-Version: 1.0" . "\r\n";
    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-   $headers = "From:" . $from;
-   if(mail($to,$subject,$message, $headers)) {
-      echo "<h1>Hello message was sent.</h1>";
+   $headers .= "From:" . $from;
+   $sent = mail($to,$subject,$message, $headers);
+   if($sent) {
+      echo "<h1>Hello ".$_POST['name']." message was sent.</h1>";
    } else {
-      echo "<h1>Error: Message was not sent.</h1>";
+      echo "<h1>Error: Message was not sent. ".$_POST['name'].", tell Kaji</h1>";
    }
 ?>
