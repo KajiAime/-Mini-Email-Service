38599b5d813f0c985e8f11308ec598dbac45a091
Nde-Ebom <ndekaji@gmail.com>
2022-08-10 17:46:24 +0100

    The website sends a mail to someone else.
    If you use this script, my email will be used as the

--- ack.php
+++ ack.php
@@ -1,17 +1,16 @@
 <?php
     if(isset($_POST['send'])){
-        $to='ndekaji@gmail.com';
         $subject='Feedback from the website';
         $message='Name: '.$_POST['name']."\r\n\r\n";
         $email=filter_input(INPUT_POST,'email',FILTER_VALIDATE_EMAIL);
-        $headers="From: Your friend \r\n";
+        $headers="From: ".$_POST['name']." \r\n";
         $headers.='Content-type: text/plain; charset: utf-8';
         if($email){
-            $headers.="\r\nReply_to: $email";
+            $headers.="\r\nReply_to: $_POST['name']";
         }
-        $message.='Email: '.$_POST['email']."\r\n\r\n";
+        $message.='Your email address: '.$_POST['email']."\r\n\r\n";
         $message.='Comments: '.$_POST['comment'];
-        $success=mail($email,$subject,$message,$headers,'-fndekaji@gmail.com');
+        $success=mail($email,$subject,$message,$headers,'-f'.$email);
     }
 ?>
 <!DOCTYPE html>
@@ -20,7 +19,8 @@
         <title>🙂 Sending emails</title>
     </head>
     <body>
-        <?php if(isset($success) && $success){?>
+        <?php if(isset($success) && $success){
+        echo"Hello".$_POST['name']." ";?>
         <h1>Thank you for your response. Kaji appreciates.</h1>
         <?php }else{?>
         <h1>Errors: Something went wrong while sending. Please tell Kaji.</h1>
